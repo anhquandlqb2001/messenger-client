386afdf49fe320d84cc6fe312f7d963c4b61add9
dongle.qb.2001@gmail.com <dongle.qb.2001@gmail.com>
2021-06-28 10:45:25 +0700

    using axios interceptor

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,25 @@ import "./App.css";
 import PrivateRoute from "./helpers/PrivateRoute";
 import LandingPage from "./pages/LandingPage";
 import Message from "./pages/Message";
-
+import axios from "axios";
+import useLocalStorage from "./helpers/useLocalStorage";
+import { useEffect } from "react";
 function App() {
+  const [token, setToken] = useLocalStorage("token");
+  axios.defaults.baseURL = "http://localhost:5000";
+  useEffect(() => {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  }, [token]);
+
+  axios.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response.status === 401) {
+        setToken(null);
+      }
+    }
+  );
+
   return (
     <Router>
       <Switch>
